395fb9813bc1811d04b92086ee331ec41aac8971
Loubna ALFIDI <loubna.alfidi@gmail.com>
2023-08-09 15:13:09 -0700

    shell

---
+++ shell.h
@@ -0,0 +1,17 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+/* Libraries */
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <limit.h>
+#include <fcntl.h>
+#include <errno.h>
+
+/* Macros */
+#define BUFFER_SIZE 1024
+
+/* string functions */
