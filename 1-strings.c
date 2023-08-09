d0d17bc5f92938763daa4881d60b71825894a95c
root <HichamLuffy>
2023-08-09 15:41:16 -0700

    strings

--- 1-strings.c
+++ 1-strings.c
@@ -87,3 +87,20 @@ char *_strdup(char *str)
 		a[j] = str[j];
 	return (a);
 }
+/**
+ * _strchr - entry point
+ * @s: input
+ * @c: input
+ * Return: Always 0 (Success)
+ */
+char *_strchr(char *s, char c)
+{
+        int i = 0;
+
+        for (; s[i] >= '\0'; i++)
+        {
+                if (s[i] == c)
+                        return (&s[i]);
+        }
+        return (0);
+}
