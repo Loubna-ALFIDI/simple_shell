18648aedb7739a57a08c4e29c23b602a49a70002
Loubna ALFIDI <loubna.alfidi@gmail.com>
2023-08-14 12:47:29 -0700

    gg

--- handl.c
+++ handl.c
@@ -6,12 +6,21 @@
  */
 void echo(char *cmd)
 {
-	char c;
+	/*char c; */
 	char *str = cmd + _strlen("echo ");
-	int str_length = _strlen(str), i;
+/**
+ * int str_length = _strlen(str), i;
 
-	for (i = 0; i < str_length; i++)
-		write(1, &c, 1);
-	write(1, "\n", 1);
+ *	for (i = 0; i < str_length; i++)
+ *		write(1, &c, 1);
+ *	write(1, "\n", 1);
+ */
+	  while (*str != '\0') {
+        write(1, str, 1);
+        str++;
+    }
+
+    write(1, "\n", 1);
+
 }
 
