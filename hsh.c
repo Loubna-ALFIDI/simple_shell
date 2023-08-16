cb889bcde4937d4183ca94f79a76b6337f109004
root <HichamLuffy>
2023-08-15 17:18:58 -0700

    idk

--- hsh.c
+++ hsh.c
@@ -1,60 +1,18 @@
 #include "shell.h"
-
-/**
- * split_input - Split input command into arguments.
- *
- * @cmd: Input command string.
- * @argc: Pointer to store the argument count.
- *
- * Return: Array of arguments.
- */
 char **split_input(char *cmd, int *argc)
 {
+    char *str;
     char **argv = NULL;
     int count = 0;
-    int in_arg = 0;
-    char arg_buffer[512];
-    size_t i;
 
-    for (i = 0; cmd[i]; i++)
+    str = strtok(cmd, " ");
+    while (str)
     {
-        if (cmd[i] == ' ' && !in_arg)
-        {
-            continue;
-        }
-        else if (cmd[i] == ' ' && in_arg)
-        {
-            arg_buffer[count] = '\0';
-            argv = realloc(argv, (count + 1) * sizeof(char *));
-            argv[count] = _strdup(arg_buffer);
-            count++;
-            in_arg = 0;
-        }
-	else if (cmd[i] == '"')
-        {
-            i++;
-            while (cmd[i] && cmd[i] != '"')
-            {
-                arg_buffer[in_arg] = cmd[i];
-                in_arg++;
-                i++;
-            }
-        }
-        else
-        {
-            arg_buffer[in_arg] = cmd[i];
-            in_arg++;
-        }
-    }
-
-    if (in_arg)
-    {
-        arg_buffer[in_arg] = '\0';
         argv = realloc(argv, (count + 1) * sizeof(char *));
-        argv[count] = _strdup(arg_buffer);
+        argv[count] = _strdup(str);
         count++;
+        str = strtok(NULL, " ");
     }
-
     argv = realloc(argv, (count + 1) * sizeof(char *));
     argv[count] = NULL;
 
@@ -63,64 +21,68 @@ char **split_input(char *cmd, int *argc)
 }
 int main(void)
 {
-	char *cmd = NULL;
-	size_t n = 0;
-	char *found_path;
-	pid_t pid;
-	int argc = 0, status, j;
-	char **argv = NULL;
+        char *cmd = NULL;
+        size_t n = 0;
+        char *found_path;
+        pid_t pid;
+        int argc = 0, status, j;
+        char **argv = NULL;
 
-	while (1)
-	{
-		write(1, "$ ", 2);
-		if (getline(&cmd, &n, stdin) == -1)
-		{
-			perror("getline");
-			break;
-		}
-		cmd[strcspn(cmd, "\n")] = '\0';
-		if (_strcmp(cmd, "exit") == 0)
-			break;
-		found_path = NULL;
-		argv = split_input(cmd, &argc);
-		if (argc > 0)
-		{
-			found_path = get_path(argv[0]);
-			if (found_path == NULL)
-			{
-				printf("command not found\n");
-				for (j = 0; j < argc; j++)
-					free(argv[j]);
-				free(argv);
-				continue;
-			}
-		}
-		if (cmd != NULL)
-		{
-		pid = fork();
-		if (pid == -1)
-		{
-		perror("fork");
-		exit(EXIT_FAILURE);
-		}
-		if (pid == 0)
-		{
-			execute_command(found_path ? found_path : argv[0], argv);
-			exit(EXIT_SUCCESS);
-		}
-		if (waitpid(pid, &status, 0) == -1)
-		{
-			perror("wait");
-			exit(EXIT_FAILURE);
-		}
-		}
-		for (j = 0; j < argc; j++)
-			free(argv[j]);
-		free(argv);
-		argc = 0;
+        while (1)
+        {
+                write(1, "$ ", 2);
+                if (getline(&cmd, &n, stdin) == -1)
+                {
+                        perror("getline");
+                        break;
+                }
+                cmd[strcspn(cmd, "\n")] = '\0';
+                if (_strcmp(cmd, "exit") == 0)
+                        break;
+                found_path = NULL;
+                argv = split_input(cmd, &argc);
+                if (argc > 0)
+                {
+                        found_path = get_path(argv[0]);
+                        if (found_path == NULL)
+                        {
+                                printf("command not found\n");
+                                for (j = 0; j < argc; j++)
+                                        free(argv[j]);
+                                free(argv);
+                                continue;
+                        }
+                }
+                if (cmd != NULL)
+                {
+                pid = fork();
+                if (pid == -1)
+                {
+                perror("fork");
+                exit(EXIT_FAILURE);
+                }
+                if (pid == 0)
+                {
+                        if (execve(found_path ? found_path : argv[0], argv, NULL) == -1)
+                        {
+                                perror("execve");
+                                exit(EXIT_FAILURE);
+                        }
+                        exit(EXIT_SUCCESS);
+                }
+                if (waitpid(pid, &status, 0) == -1)
+                {
+                        perror("wait");
+                        exit(EXIT_FAILURE);
+                }
+                }
+                for (j = 0; j < argc; j++)
+                        free(argv[j]);
+                free(argv);
+                argc = 0;
 
-	}
-	free(cmd);
+        }
+        free(cmd);
 
     return 0;
 }
--- path.c
+++ path.c
@@ -2,88 +2,72 @@
 
 char *get_path(char *command)
 {
-    char *path, *path_copy, *path_token, *file_path;
-    struct stat buffer;
+        char *path, *path_copy, *path_token, *file_path;
+        struct stat buffer;
 
-    if (stat(command, &buffer) == 0)
-        return _strdup(command);
-
-    path = getenv("PATH");
-    if (path == NULL)
-        return NULL;
-
-    path_copy = strdup(path);
-    if (path_copy == NULL)
-        return NULL;
-
-    path_token = strtok(path_copy, ":");
-
-    while (path_token != NULL)
-    {
-        file_path = malloc(strlen(path_token) + strlen(command) + 2);
-        if (file_path == NULL)
+        if (stat(command, &buffer) == 0)
+                return _strdup(command);
+        path = getenv("PATH");
+        if (path == NULL)
+                return NULL;
+        path_copy = strdup(path);
+        if (path_copy == NULL)
+                return NULL;
+        if (path)
         {
-            perror("malloc");
-            free(path_copy);
-            return NULL;
-        }
+                path_copy = _strdup(path);
 
-        _strcpy(file_path, path_token);
-        _strcat(file_path, "/");
-        _strcat(file_path, command);
+                path_token = strtok(path_copy, ":");
 
-        if (stat(file_path, &buffer) == 0)
-        {
-            free(path_copy);
-            return file_path;
+                while(path_token != NULL)
+                {
+                        /* file_path = malloc(directory_length + command_length + 2);*/
+                        file_path = malloc(strlen(path_token) + strlen(command) + 2);
+                        if (file_path == NULL)
+                        {
+                                perror("malloc");
+                                free(path_copy);
+                                return NULL;
+                        }
+                        _strcpy(file_path, path_token);
+                        _strcat(file_path, "/");
+                        _strcat(file_path, command);
+                        _strcat(file_path, "\0");
+                        if (stat(file_path, &buffer) == 0)
+                        {
+                                free(path_copy);
+                                return (file_path);
+                        }
+                        else
+                        {
+                                free(file_path);
+                                path_token = strtok(NULL, ":");
+                        }
+                }
+                free(path_copy);
+                if (stat(command, &buffer) == 0)
+                {
+                        return (command);
+                }
+                return (NULL);
         }
-        else
-        {
-            free(file_path);
-            path_token = strtok(NULL, ":");
-        }
-    }
-
-    free(path_copy);
-    return NULL;
+        free(path_copy);
+        return (NULL);
 }
 
 /**
  * execute_command - executes the given command
  * argv: command
  */
-void execute_command(char *executable, char **argv)
+void execute_command(char **argv)
 {
-    extern char **environ;
-    char *built_in_path;
-    int i;
+        char *executable = argv[0];
+        extern char **environ;
 
-    if (_strcmp(executable, "echo") == 0)
-    {
-        for (i = 1; argv[i] != NULL; i++)
+        if (execve(executable, argv, environ) == -1)
         {
-            printf("%s ", argv[i]);
+                perror("./shell");
+                exit(EXIT_FAILURE);
         }
-        printf("\n");
-    } else
-    {
-	    built_in_path = get_path(executable);
-	    if (built_in_path != NULL)
-	    {
-		    if (execve(built_in_path, argv, environ) == -1)
-		    {
-			    perror(built_in_path);
-			    exit(EXIT_FAILURE);
-		    }
-            free(built_in_path);
-	    }else
-	    {
-		if (execve(executable, argv, environ) == -1)
-		{
-			perror("executable");
-			exit(EXIT_FAILURE);
-		}
-	    }
-    }
-    free(environ);
+        free(environ);
 }
